72af36a93b73bcbcb20fc7def82dff15358db900
Aishaabdi1 <aishahajisirad@gmail.com>
2022-08-28 00:25:34 +0100

    complete html routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
-// 'Get /notes'  should return the notes.html file.
+// 'Get /notes'  should return the notes.html file
 main2.get ("/notes", (req, res) => 
 res.sendFile(path.join(__dirname, "../public/notes.html"))
 );
 
+// 'Get *' should return the notes.html file as default
+main2.get ("*", (req, res) => 
+res.sendFile(path.join(__dirname, "../public/index.html"))
+);
+
+module.exports = main2;
