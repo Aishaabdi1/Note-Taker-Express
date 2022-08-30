33f66b500cc57c1b25764a5deed828b0a0c27a73
Aishaabdi1 <aishahajisirad@gmail.com>
2022-08-30 19:26:30 +0100

    functions appends content

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -11,3 +11,25 @@ const readFromFile = util.promisify(fs.readFile);
  *  @returns {void} Returns empty
  */
 
+ fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+ err ? console.error(err) : console.info(`\nData written to ${destination}`)
+);
+/**
+*  Function reads data from a given a file and appends content
+*  @param {object} content The content you want to append to the file.
+*  @param {string} file The path to the file you want to save to.
+*  @returns {void} Returns empty
+*/
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+  
+  module.exports = { readFromFile, writeToFile, readAndAppend };
