0d701a0c7958538b3febd7f02cb9c1bb5e3fffaf
Aishaabdi1 <aishahajisirad@gmail.com>
2022-08-30 12:10:38 +0100

    fixing errors in htmlroutes.js

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,14 +1,15 @@
-const fs = require('fs');
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const express = require("express");
+const router = express.Router();
+const path = require("path");
 
 // 'Get /notes'  should return the notes.html file
-main2.get ("/notes", (req, res) => 
-res.sendFile(path.join(__dirname, "../public/notes.html"))
-);
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
 
 // 'Get *' should return the notes.html file as default
-main2.get ("*", (req, res) => 
-res.sendFile(path.join(__dirname, "../public/index.html"))
-);
-
-module.exports = main2;
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  
+  module.exports = router;
